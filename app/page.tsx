c0c5f724db6463f8c117420cdbbc677ca2c52421
xsantanax <rafaelsantana111@gmail.com>
2023-02-15 13:50:26 -0300

    add @heroicons/react

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import { SunIcon } from "@heroicons/react/24/outline";
+
 function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
       <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
       <div>
         <div className="flex flex-col items-center justify-center mb-5">
+          <SunIcon className="h-8 w-8" />
+          Examples
+        </div>
+        <div className="space-y-2">
           <p className="infoText">Explain something to me</p>
           <p className="infoText">
             What is the difference between a dog and a cat?
