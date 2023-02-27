3d21972407b6573f39f4eb18de6cf61f4820ae50
xsantanax <rafaelsantana111@gmail.com>
2023-02-27 13:20:42 -0300

    use session

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+"use client";
+import { useSession, signOut } from "next-auth/react";
 import NewChat from "./NewChat";
 
 function Sidebar() {
+  const { data: session } = useSession();
+
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -11,6 +14,14 @@ function Sidebar() {
           {/* Map through the ChatRows */}
         </div>
       </div>
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image!}
+          alt="Proile pic"
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+        />
+      )}
     </div>
   );
 }
